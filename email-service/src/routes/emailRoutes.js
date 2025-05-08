da493b4bcc2af8edfc9e798cc2219d4afb63fce7
SHARMA Mohit <mohit.sharma.2001rip@gmail.com>
2025-05-08 16:27:16 +0530

    modified the emailroutes.js

--- email-service/src/routes/emailRoutes.js
+++ email-service/src/routes/emailRoutes.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const { sendEmail } = require("../controllers/emailController");
-const authenticate = require("../utils/authenticate");
 
 router.post("/send", sendEmail);
 
